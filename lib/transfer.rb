e83c1cd8099408ae532227821106fb7be73916b1
Taylor Williams <taylor@thewilliams.net>
2018-06-05 21:12:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,8 @@ class Transfer
         @receiver.balance += @amount
         @status = "complete"
       end
+    else
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
